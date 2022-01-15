a90253651126a497799fda4739e017b215eb69f3
djwelsh98 <dustin_welsh98@yahoo.com>
2022-01-15 12:51:18 -0700

    axios fixed

--- chatroom/server/controllers/test.js
+++ chatroom/server/controllers/test.js
@@ -26,11 +26,11 @@
 // }
 
 export const getTest = async (req, res) => {
-    res.status(200).json('successful get');
+    await res.status(200).json('successful get');
     console.log('get works')
 }
 
 export const postTest = async (req, res) => {
-    res.status(201).json('successful post');
+    await res.status(201).json('successful post');
     console.log('post works')
 }
--- chatroom/server/index.js
+++ chatroom/server/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/chats', ChatRoutes)
-app.use('test', TestRoutes)
+app.use('/test', TestRoutes)
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
