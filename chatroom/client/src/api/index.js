a30b2b2338e348f7f903bd771f414cc41cf1ed0e
djwelsh98 <dustin_welsh98@yahoo.com>
2022-01-22 13:48:09 -0700

    unfucked it

--- chatroom/client/src/api/index.js
+++ chatroom/client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API = axios.create({baseURL: "http://localhost:5000"})
-const API = axios.create(process.env.baseURL)
+const API = axios.create({baseURL: "http://localhost:5000"})
+// const API = axios.create(process.env.baseURL)
 
 export const testGet = () => API.get('/test/get');
 
