375cba313c08cd4fdeac70630d2cc31c7fc6e5ca
djwelsh98 <dustin_welsh98@yahoo.com>
2022-01-23 11:24:40 -0700

    piece of shit code

--- chatroom/client/src/pages/userPage.js
+++ chatroom/client/src/pages/userPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { Container, Row, Col, Form, Button } from "react-bootstrap"
+import { Container, Row, Col, Form, Button, TabContent } from "react-bootstrap"
 import avatar from '../assets/placeholder.png'
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { redux_addUsers } from '../state/userSlice';
+import { addUsers, getUsers } from '../actions';
 import AccountModal from '../AccountModal'
 const Username = 'username'
 const FavColor = 'favcolor'
@@ -10,6 +13,32 @@ function UserPage() {
     const [show, setShow] = useState(false); //sets show to false, which keeps the Modal from loading
     const handleShow = () => setShow(true); //function to change show to true and load Modal
     const handleClose = () => setShow(false);
+    const dispatch = useDispatch();
+    const content = useSelector(addUsers);
+
+    //user db
+    const [response, setResponse] = useState()
+    const updateField = (e) => {
+        setResponse({
+            ...response,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleClose();
+        const updateContentArr = [
+            ...content, response
+        ];
+        if (response.userSchema) {
+            dispatch(addUsers(updateContentArr))
+            setResponse({})
+        }
+    }
+
+    useEffect(() => {
+        dispatch(getUsers()) //triggers the db call
+    }, [dispatch]);
 
 
     return (
@@ -18,16 +47,18 @@ function UserPage() {
                 <Row className="justify-content-md-center">
                     <Col className="top" xs lg="6">
                         <div class='box'>
-                            <Form>
-                                <Form.Label className='miniMargin'><img className='avatar' src={avatar} alt='' /></Form.Label>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Label className='miniMargin' onChange={updateField} name="avatar"><img className='avatar' src={avatar} alt='' /></Form.Label>
                                 <Form.Text className='miniMargin'><h3>{Username}</h3></Form.Text>
+                                <Form.Control className='miniMargin' type="Username" placeholder="Password" name="userName" onChange={updateField}/>
                                 <Form.Label className='miniMargin'><h3>Password</h3></Form.Label>
-                                <Form.Control className='miniMargin' type="Password" placeholder="Password" />
+                                <Form.Control className='miniMargin' type="Password" placeholder="Password" name="password" onChange={updateField}/>
                                 <Form.Text className='miniMargin'><h3>{FavColor}</h3></Form.Text>
-                                <Form.Control className='miniMargin' type="Color" placeholder="Favorite Color" />
+                                <Form.Control className='miniMargin' type="Color" placeholder="Favorite Color" naem="favoriteColor" onChange={updateField}/>
                                 <Button onClick={(e) => {
-
+                                    
                                     e.preventDefault();
+                                    handleSubmit()
                                     handleShow()
                                 }}>Submit here</Button>
                             </Form>
